5026f17f85ef131401d6edd25379d4664f256f11
Slyvred <52755677+Slyvred@users.noreply.github.com>
2024-09-05 11:13:35 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running fsck on /dev/nvme0n1p1..."
-sudo fsck /dev/nvme0n1p1 | echo -e "3\n1\n1\n1"
+echo -e "3\n1\n1\n1" | sudo fsck /dev/nvme0n1p1
 echo "done."
 
 echo "Mounting filesystems..."
@@ -10,7 +10,6 @@ sudo mount /dev/nvme0n1p1 /mnt/boot
 echo "done."
 
 echo "Entering NixOS installation..."
-sudo nixos-enter
+echo "NIXOS_INSTALL_BOOTLOADER=1 /nix/var/nix/profiles/system/bin/switch-to-configuration boot" | sudo nixos-enter
 echo "Rebuilding bootloader..."
-NIXOS_INSTALL_BOOTLOADER=1 /nix/var/nix/profiles/system/bin/switch-to-configuration boot
 echo "done."
